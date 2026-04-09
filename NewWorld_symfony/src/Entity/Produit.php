4e95a544ee44ccbf179389111f4ebb3b69c7549c
kirio7 <leoburban27@gmail.com>
2026-04-09 17:44:15 +0200

    api installer et ajouter a produit

--- NewWorld_symfony/src/Entity/Produit.php
+++ NewWorld_symfony/src/Entity/Produit.php
@@ -6,7 +6,9 @@ use App\Repository\ProduitRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
 
+#[ApiResource()]
 #[ORM\Entity(repositoryClass: ProduitRepository::class)]
 class Produit
 {
