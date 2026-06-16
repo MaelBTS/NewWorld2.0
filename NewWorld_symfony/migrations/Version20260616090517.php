5486f8dd307c1df78ae070fa8f07b1b9168c5c32
kirio7 <leoburban27@gmail.com>
2026-06-16 11:12:10 +0200

    version final (migration corrigé)

--- NewWorld_symfony/migrations/Version20260616090517.php
+++ NewWorld_symfony/migrations/Version20260616090517.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260615085010 extends AbstractMigration
+final class Version20260616090517 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20260615085010 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE archives (id INT AUTO_INCREMENT NOT NULL, type VARCHAR(255) NOT NULL, ancien_id INT NOT NULL, data LONGTEXT NOT NULL, date_archivage VARCHAR(255) NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE categorie (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE archives');
+        $this->addSql('DROP TABLE categorie');
     }
 }
